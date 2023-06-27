4e1dc507abaccce2d3cf33e120d3573550e37312
ChristianAlteri <alteri.christian@gmail.com>
2023-06-27 20:30:47 +0800

    models(User, Side) created --- Seed(User,Side) created

--- models/Side.js
+++ models/Side.js
@@ -1,6 +1,28 @@
-// Side{
-    // Id: INT
-    // Type: Mother, Father, Spouse, Sibling
-    // 
-    // 
-// }
+const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require('../config/connection');
+
+class Side extends Model {}
+
+Side.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = Side;
