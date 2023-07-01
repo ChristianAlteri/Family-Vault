736f520f4b322cd9831e976ac8cfd2709fc9e655
Frank Ocean <chasebarrettbrown@hotmail.com>
2023-07-01 21:29:51 +0800

    added an api route "auth"

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,80 @@
+// routes/auth.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const { User } = require('../models'); // Import the User model
 
-//need authentication route............ 
-
-
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
+// GET /auth
+router.get('/', async (req, res) => {
   try {
-    // find the user by email
-    const user = await User.findOne({ where: { email } });
-
-    // see if user exists
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    // see if the pw is correct
-    const passwordMatch = user.checkPassword(password);
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid password' });
-    }
-
-
-    res.status(200).json({ message: 'Login successful' });
+    const users = await User.findAll(); // Retrieve all users from the database
+    res.json(users);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
-//login
-//signout
-//s
+// POST /auth
+router.post('/', async (req, res) => {
+  try {
+    const newUser = req.body;
+    const createdUser = await User.create(newUser); // Create a new user in the database
+    res.json(createdUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// GET /auth/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId); // Retrieve a specific user by ID
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// PUT /auth/:id
+router.put('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const updatedUser = req.body;
+    const user = await User.findByPk(userId); // Retrieve the user by ID
+    if (user) {
+      await user.update(updatedUser); // Update the user in the database
+      res.json({ message: 'User updated successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// DELETE /auth/:id
+router.delete('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId); // Retrieve the user by ID
+    if (user) {
+      await user.destroy(); // Delete the user from the database
+      res.json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const homeRoutes = require('./home');
 const authRoutes = require('./auth');
 
-router.use('/', homeRoutes);
-router.use('/login', authRoutes);
+//router.use('/', homeRoutes);
+router.use('/auth', authRoutes);
 
 module.exports =router;
