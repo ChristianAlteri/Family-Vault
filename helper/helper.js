aa5e796590b40723c334e56b3fd81482d84c5065
ChristianAlteri <alteri.christian@gmail.com>
2023-07-01 12:27:35 +0800

    added linkNode function

--- helper/helper.js
+++ helper/helper.js
@@ -1,4 +1,5 @@
 
+const { and } = require('sequelize');
 const { User, Relationship } = require('../models')
 
 // Relation to user from new user
@@ -22,7 +23,7 @@ const createRelationToLoggedInUser = async (req, res, newUser) => {
       generation = req.body.generation + 1;
     }
     console.log(newUser);
-    console.log('here');
+
     // Create the relationship record in the "relationships" table
     relationData = await Relationship.create({
       user_id: req.body.user_id,
@@ -31,16 +32,46 @@ const createRelationToLoggedInUser = async (req, res, newUser) => {
       source_id: req.body.source_id,
       side_id: req.body.side_id,
     });
-    
+
     console.log(relationData);
-    res.status(200).json(relationData);
+    // res.status(200).json(relationData);
+    await linkNode(req, res, newUser, relationData, generation)
+    
   } catch (err) {
     res.status(400).json(err);
   }
 };
 
-
-  module.exports = { createNode, createRelationToLoggedInUser }
+const linkNode = async (req, res, newUser, relationData, generation) => {
+  try {
+    let linkedSide;
+console.log('here');
+    if (req.body.side_from_sex === 0 && req.body.side_id === 5) {
+        linkedSide = 2;
+    } else if (req.body.side_from_sex === 1 && req.body.side_id === 5) {
+        linkedSide = 1;
+    } else if (req.body.side_id === 3) {
+        linkedSide = 3;
+    } else if (req.body.side_id === 4) {
+        linkedSide = 4;
+    } else {
+        linkedSide = 5;
+    } 
+    
+    linkedRelationData = await Relationship.create({
+      user_id: newUser.id,
+      who_related_id: req.body.user_id,
+      generation: generation - 2,
+      source_id: req.body.user_id,
+      side_id: linkedSide
+    });
+    console.log(linkedRelationData);
+    // res.status(200).json(linkedRelationData, relationData);
+  } catch (error) {
+    res.status(400).json(err);
+  }
+};
+  module.exports = { createNode, createRelationToLoggedInUser, linkNode }
   
 
 // RElations to new user from logged in
