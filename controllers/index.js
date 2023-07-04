8a8b590daa38a2a6af5ed02620431cf018f94f89
ChristianAlteri <alteri.christian@gmail.com>
2023-07-04 19:54:28 +0800

    added card functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const baseRoutes = require('./home');
 const apiRoutes = require('./api/createUser');
+const apiCard = require('./api/displayCard');
 
 router.use('/', baseRoutes);
 router.use('/', apiRoutes);
+router.use('/', apiCard);
+
 
 module.exports = router;
